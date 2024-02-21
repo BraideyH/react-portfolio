a54be3fa7d549ebfe2ecfbb8f1e558b03e099ad0
BraideyH <braideyhooper@gmail.com>
2024-02-21 00:59:13 -0600

    finished adding project screenshots and descriptions.

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import myImage from './assets/picture-of-a-very-hireable-person.png';
+import trafficJam from './assets/traffic-jam.png';
+import careerCanvas from './assets/career-canvas.png';
+import logoMaker from './assets/SVG-logo-maker.png';
+import passwordGenerator from './assets/password-generator.png';
+import codingQuiz from './assets/coding-quiz.png';
+import portfolio from './assets/portfolio.png';
 
 function App() {
   return (
@@ -23,43 +29,51 @@ function App() {
       </header>
       <div className="about-me">
         <h2>About Me</h2>
-        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore voluptas, consequuntur eum dignissimos quia dolorum ab! Vel, impedit expedita! Totam eius tempora enim nam vitae! Ipsum expedita repellendus doloribus consequuntur?</h3>
+        <h3 className="about-me-text">Hello! I'm Braidey, and I'm on an exciting journey transitioning from a background in emergency healthcare to the dynamic world of web development. While I may not have professional coding experience yet, my passion for technology and problem-solving has been a driving force behind my decision to pursue this new career path. I am currently immersing myself in learning various programming languages, frameworks, and technologies to build modern and user-friendly web applications. Through self-directed learning, online courses, and coding projects, I am developing my skills in front-end and back-end development. My background in emergency healthcare has equipped me with valuable skills in critical thinking, adaptability, and attention to detail – qualities that I am eager to bring to my new role as a web developer. I'm excited to embark on this new chapter and look forward to the opportunities and experiences that lie ahead. Feel free to explore my portfolio to see some of my coding projects and reach out if you'd like to connect or collaborate! </h3>
       </div>
       <div className='projects-text'>
         <h2>Projects</h2>
         <div className="project-cards">
-        <div className="card">
-          <img src="" alt="Project 1" />
-          <h2>Project 1</h2>
-          <p>Description of Project 1</p>
+        <div className="card" >
+          <img src={trafficJam} alt="Traffic-Jam app screenshot" />
+          <h2>Traffic Jam</h2>
+          <p>Traffic Jam shows users live events around roadtrip destinations using Google Maps API and Ticketmaster API tools</p>
         </div>
         <div className="card">
-          <img src="" alt="Project 2" />
-          <h2>Project 2</h2>
-          <p>Description of Project 2</p>
+          <img src={careerCanvas} alt="Career Canvas" />
+          <h2>Career Canvas</h2>
+          <p>Career Canvas provides tools for users to track job applications and include notes and statuses that flow into charts dynamically.</p>
         </div>
         <div className="card">
-          <img src="" alt="Project 3" />
-          <h2>Project 3</h2>
-          <p>Description of Project 3</p>
+          <img src={logoMaker} alt="Logo Maker" />
+          <h2>SVG Logo Maker</h2>
+          <p>This simple backend app generates a logo image based on input provided by the user such as text, color, and shape.</p>
         </div>
         <div className="card">
-          <img src="" alt="Project 4" />
-          <h2>Project 4</h2>
-          <p>Description of Project 4</p>
+          <img src={passwordGenerator} alt="Password Generator" />
+          <h2>Password Generator</h2>
+          <p>This password generator will provide users with random passwords within defined parameters such as character inclusions and string length.</p>
         </div>
         <div className="card">
-          <img src="" alt="Project 5" />
-          <h2>Project 5</h2>
-          <p>Description of Project 5</p>
+          <img src={codingQuiz} alt="Coding Quiz" />
+          <h2>Coding Quiz</h2>
+          <p>This simple app functions as a quiz for new developers to test their knowledge.</p>
         </div>
         <div className="card">
-          <img src="" alt="Project 6" />
-          <h2>Project 6</h2>
-          <p>Description of Project 6</p>
+          <img src={portfolio} alt="Portfolio" />
+          <h2>React Portfolio</h2>
+          <p>Take a look under the hood of this application!</p>
         </div>
         </div>
         </div>
+        <div className="about-me">
+        <h2>Contact Me</h2>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore voluptas, consequuntur eum dignissimos quia dolorum ab! Vel, impedit expedita! Totam eius tempora enim nam vitae! Ipsum expedita repellendus doloribus consequuntur?</h3>
+      </div>
+      <div className="about-me">
+        <h2>Resume</h2>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore voluptas, consequuntur eum dignissimos quia dolorum ab! Vel, impedit expedita! Totam eius tempora enim nam vitae! Ipsum expedita repellendus doloribus consequuntur?</h3>
+      </div>
       </div>
   )
 }
